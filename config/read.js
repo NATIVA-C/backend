5f2d2787e0db00cae17d843315dcd4dcab6e85a7
j.zhou <52964886+jiu-z@users.noreply.github.com>
2023-07-13 22:13:09 +0900

    update

--- config/read.js
+++ config/read.js
@@ -3,45 +3,68 @@ const fs = require("fs");
 const path = require("path");
 
 // 去除空格
-const removeBlock = (txt) => {
-  if (txt && typeof txt === "string") {
-    return txt.replace(/[\t|\n]*/gi, "");
+const regex_Logo_1 = (txt) => {
+  const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
+  const matches = txt.match(regex);
+
+  if (matches) {
+    return true;
   } else {
-    return txt;
+    return false;
   }
+};
+
+const regex_Logo_2 = (txt) => {
+  const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
+  const matches = txt.match(regex);
+
+  if (matches) {
+    const link = matches[1];
+  }
+  // else {
+  //   console.log('未找到匹配项.');
+  // }
 };
 
 // crawl
 module.exports = async (dir, name, extra) => {
-  // 获取网源的页面内容
-  // let crawlUrl = "https://search.jd.com/Search?keyword=" + keyword;
-  // // 解析页面内容 cheerio 类似于jquery
-  // const $ = cheerio.load(data);
-  // let arr = []; // 存储列表
-  // 读取文件的路径
-  // const filePath = "path/to/your/name.md";
-  // 读取文件的相对路径
-  console.log(__dirname);
-  const filePath = path.join(__dirname, '..', '..','ctd','md',dir, name);
-  console.log(filePath);
-    // 创建可读流
-    const stream = fs.createReadStream(filePath);
+  let arr = []; 
+  let preArr = [];
+  
+  const filePath = path.join(__dirname, "..", "..", "ctd", "md", dir, name);
+  // 创建可读流
+  const stream = fs.createReadStream(filePath);
+  // 创建逐行读取的接口
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity, // 保留换行符
+  });
+  const outArr = [];
+  // 逐行读取文件内容
+  rl.on("line", (line) => {
+    if (regex_Logo_1(line)) {
+      console.log("aa");
+      outArr.push("## " + name);
+      preArr.push(line);
+    }
 
-    // 创建逐行读取的接口
-    const rl = readline.createInterface({
-      input: stream,
-      crlfDelay: Infinity, // 保留换行符
-    });
+    // console.log(line); // 在这里处理每行的内容
+    if (line != null) {
+      arr.push(line);
+    }
+  });
 
-    // 逐行读取文件内容
-    rl.on("line", (line) => {
-      console.log(line); // 在这里处理每行的内容
-    });
-
-    // 读取完成时触发的事件
-    rl.on("close", () => {
-      console.log("文件读取完成");
-    });
-
+  // 读取完成时触发的事件
+  rl.on("close", () => {
+    fileOutput(outArr);
+    console.log("文件读取完成");
+  });
   return "over" + extra + name + dir;
+};
+
+const fileOutput = (outArr) => {
+  // 遍历列表
+  outArr.forEach((item) => {
+    console.log(item);
+  });
 };
