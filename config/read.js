32a26b72690f94181bbb361714fc2a760a08f91e
j.zhou <52964886+jiu-z@users.noreply.github.com>
2023-07-13 19:58:11 +0900

    up

--- config/read.js
+++ config/read.js
@@ -24,24 +24,24 @@ module.exports = async (dir, name, extra) => {
   console.log(__dirname);
   const filePath = path.join(__dirname, '..', '..','ctd','md',dir, name);
   console.log(filePath);
-  //   // 创建可读流
-  //   const stream = fs.createReadStream(filePath);
+    // 创建可读流
+    const stream = fs.createReadStream(filePath);
 
-  //   // 创建逐行读取的接口
-  //   const rl = readline.createInterface({
-  //     input: stream,
-  //     crlfDelay: Infinity, // 保留换行符
-  //   });
+    // 创建逐行读取的接口
+    const rl = readline.createInterface({
+      input: stream,
+      crlfDelay: Infinity, // 保留换行符
+    });
 
-  //   // 逐行读取文件内容
-  //   rl.on("line", (line) => {
-  //     console.log(line); // 在这里处理每行的内容
-  //   });
+    // 逐行读取文件内容
+    rl.on("line", (line) => {
+      console.log(line); // 在这里处理每行的内容
+    });
 
-  //   // 读取完成时触发的事件
-  //   rl.on("close", () => {
-  //     console.log("文件读取完成");
-  //   });
+    // 读取完成时触发的事件
+    rl.on("close", () => {
+      console.log("文件读取完成");
+    });
 
   return "over" + extra + name + dir;
 };
