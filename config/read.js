75b825de904e15f21b276d6f24336a42b6704842
andre <jiuzhou.zhou9@gmail.com>
2023-07-14 17:09:14 +0900

    update

--- config/read.js
+++ config/read.js
@@ -2,41 +2,11 @@ const readline = require("readline");
 const fs = require("fs");
 const path = require("path");
 const writer = require("./write");
-const preArr = [];
-const allArr = [];
-
-const insertData = (data) => {
-  preArr.push(data); // 插入新数据
-};
-
-
-// 匹配logo
-const regex_Logo_1 = (txt) => {
-  const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
-  const matches = txt.match(regex);
-
-  if (matches) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const regex_tile_2 = (txt) => {
-  const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
-  const matches = txt.match(regex);
-
-  if (matches) {
-    const link = matches[1];
-  }
-  // else {
-  //   console.log('未找到匹配项.');
-  // }
-};
 
 // 读取文件
 module.exports = async (dir, name, extra) => {
   let outArr = [];
+  let allArr = [];
   const filePath = path.join(__dirname, "..", "..", "ctd", "md", dir, name);
   // 创建可读流
   const stream = fs.createReadStream(filePath);
@@ -47,21 +17,17 @@ module.exports = async (dir, name, extra) => {
   });
 
   // 逐行读取文件内容
-  rl.on("line", (line) => {
-    //每行保留到内存
+  rl.on('line', (line) => {
     allArr.push(line);
-    //regex_Logo_1
-    if (regex_Logo_1(line)) {
-      outArr.push("## " + name);
-      insertData(line);
-    }
   });
 
   // 读取完成时触发的事件
   rl.on("close", () => {
-    let data = writer(outArr, filePath);
+    console.log(allArr.length);
+    let data = writer(allArr, filePath,name);
     console.log(data);
   });
+
   return "请查看" + filePath + ".tf.md";
 };
 
--- config/write.js
+++ config/write.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+
 // 将每个元素逐行写入 .md 文件
-module.exports = async (outputArr, name) => {
-    const outputPath = path.join(name + '.tr.md');
+module.exports = async (allArr, filePath,name) => {
+    let outPutArr = [];
+    for (let i = 0; i < allArr.length; i++) {
+        let txt = allArr[i];
+        //regex_Logo_1
+        if (regex_Logo_1(txt)) {
+            outPutArr.push("## " + name.replace(".md",""));
+            continue;
+        }
+
+
+        outPutArr.push(txt);
+    }
+
+    console.log("-------------------------------");
+    console.log(outPutArr.length);
+
+    const outputPath = path.join(filePath + '.tr.md');
     const stream = fs.createWriteStream(outputPath);
-    outputArr.forEach((line) => {
+    outPutArr.forEach((line) => {
         stream.write(line + '\n');
     });
 
@@ -12,3 +29,26 @@ module.exports = async (outputArr, name) => {
     return "文件写入成功";
 };
 
+// 匹配logo
+const regex_Logo_1 = (txt) => {
+    const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
+    const matches = txt.match(regex);
+
+    if (matches) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const regex_tile_2 = (txt) => {
+    const regex = /\!\[ExamTopics Logo\]\((.*?)\)/;
+    const matches = txt.match(regex);
+
+    if (matches) {
+        const link = matches[1];
+    }
+    // else {
+    //   console.log('未找到匹配项.');
+    // }
+};
